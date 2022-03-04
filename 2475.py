0e4dcb439ca9e009b6a12544ba6103a41bbbfbf4
hellojkman <sun-up12@hanmail.net>
2022-03-04 16:11:21 +0900

    code

---
+++ 2475.py
@@ -0,0 +1,4 @@
+import sys
+
+A, B, C, D, E = map(int,sys.stdin.readline().split())
+print((A**2 + B**2 + C**2 + D**2 + E**2) % 10)
---
+++ test.py
@@ -0,0 +1,4 @@
+import sys
+A = int(sys.stdin.readline())
+for i in range(1,10):
+    print( str(A) + " * " + str(i) + " = " + str(A * int(i)))
